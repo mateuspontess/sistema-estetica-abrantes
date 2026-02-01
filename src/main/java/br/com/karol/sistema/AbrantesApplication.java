368ae39067333716a3fade61d6577ecce6eedbed
Mateus Pontes <mateuspsdd@gmail.com>
2026-01-31 21:14:08 -0300

    Feat: Enable asynchronous processing support

--- src/main/java/br/com/karol/sistema/AbrantesApplication.java
+++ src/main/java/br/com/karol/sistema/AbrantesApplication.java
@@ -4,18 +4,20 @@ import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import jakarta.annotation.PostConstruct;
 
+@EnableAsync
 @SpringBootApplication
 public class AbrantesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AbrantesApplication.class, args);
 	}
-	
+
 	@PostConstruct
-	public void init(){
+	public void init() {
 		TimeZone.setDefault(TimeZone.getTimeZone("GMT-3"));
-  	}
+	}
 }
